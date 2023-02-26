161df09b3da0aa79b9b8ef0f58ae8578b850c3fa
CryptoAutistic8 <120666391+CryptoAutistic80@users.noreply.github.com>
2023-02-26 17:40:45 +0000

    Tokens = 300 Prompt Edited Nexus trained on APTOS Bearables DAO

--- bosworth.py
+++ bosworth.py
@@ -39,7 +39,7 @@ def gpt3_embedding(content, engine='text-embedding-ada-002'):
     return vector
 
 
-def gpt3_completion(prompt, engine='text-davinci-003', temp=0.7, top_p=1.0, tokens=200, freq_pen=0.25, pres_pen=0.05, stop=['USER:', 'BOSWORTH:']):
+def gpt3_completion(prompt, engine='text-davinci-003', temp=0.7, top_p=1.0, tokens=300, freq_pen=0.25, pres_pen=0.05, stop=['USER:', 'BOSWORTH:']):
     max_retry = 5
     retry = 0
     prompt = prompt.encode(encoding='ASCII',errors='ignore').decode()
